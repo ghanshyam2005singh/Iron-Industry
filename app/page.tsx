b7f5c171b9a1844b812d604a51912df82a22c7b5
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-04-21 22:42:37 +0530

    page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Products from './products/page';
+
 export default function HomePage() {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+    <section className=" bg-[#0F172A]">
+      <section className="min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-4 text-amber-300">Welcome to Iron Industry</h1>
       <p className="text-lg text-gray-600 mb-6">Forging the future through innovation and purpose-built products like AlumConnect.</p>
       <a href="/products" className="text-blue-600">Explore Our Products</a>
+      </section>
+      <section className="min-h-screen flex flex-col items-center justify-center">
+        <Products />
+        <p className="text-lg text-gray-600 mt-4">Check out our latest offerings!</p>
+      </section>
     </section>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,12 @@
 export function Navbar() {
     return (
-      <header className="py-4 px-6 flex justify-between items-center border-b">
-        <h1 className="text-shadow-cyan-950">Iron Industry</h1>
-        <nav className="space-x-4 margin-left-4">
-          <a href="/about">About</a>
-          <a href="/team">Team</a>
-          <a href="/products">Products</a>
-          <a href="/contact">Contact</a>
+      <header className=" bg-[#1E293B] py-4 px-6 flex justify-between items-center">
+        <h1 className="text-2xl font-bold text-[#F1F5F9]">Iron Industry</h1>
+        <nav className="space-x-10 ml-20 font-semibold">
+          <a href="/about" className="hover:text-[#38BDF8] text-[#22D3EE]">About</a>
+          <a href="/team" className="hover:text-[#38BDF8] text-[#22D3EE]">Team</a>
+          <a href="/products" className="hover:text-[#38BDF8] text-[#22D3EE]">Products</a>
+          <a href="/contact" className="hover:text-[#38BDF8] text-[#22D3EE]">Contact</a>
         </nav>
       </header>
     );
