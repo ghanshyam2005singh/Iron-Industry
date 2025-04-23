d6b224c25abb29bf096a530f785bd1fa7bd79dda
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-04-23 17:08:05 +0530

    changed

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -2,7 +2,7 @@ export default function TeamPage() {
     return (
       <div>
         <h2 className="text-3xl font-semibold mb-4">Meet the Team</h2>
-        <p className="text-gray-700">We're a passionate team of students, developers, designers, and community builders.</p>
+        <p className="text-gray-700">We are passionate team of students, developers, designers, and community builders.</p>
         {/* Add cards or avatars here later */}
       </div>
     );
