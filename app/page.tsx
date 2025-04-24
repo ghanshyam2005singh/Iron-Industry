f348c277a3a5c575c255f72c6a8955886beb49dc
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-04-24 19:25:01 +0530

    Page theme changed

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,18 @@ import Team from "./team/page";
 
 export default function HomePage() {
   return (
-    <section className="max-w-100% mx-auto">
-        <section className="min-h-screen flex flex-col items-center justify-center">
-      <About />
+    <section className="bg-black text-white px-4 pt-28 space-y-28">
+      <section className="min-h-screen flex items-center justify-center">
+        <About />
       </section>
-      <section className="min-h-screen flex flex-col items-center justify-center">
+      <section className="min-h-screen flex items-center justify-center">
         <Products />
-        <p className="text-lg text-gray-600 mt-4">Check out our latest offerings!</p>
       </section>
-      <section className="min-h-screen flex flex-col items-center justify-center">
-      <Team />
+      <section className="min-h-screen flex items-center justify-center">
+        <Team />
       </section>
-      <section className="min-h-screen flex flex-col items-center justify-center">
-        <Contact/>
+      <section className="min-h-screen flex items-center justify-center">
+        <Contact />
       </section>
     </section>
   );
