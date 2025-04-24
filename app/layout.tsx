2cffc8fa118c63e3517f12c113e71bdc5caf8c10
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-04-24 19:24:40 +0530

    Page theme changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,8 @@ import "./globals.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="font-sans bg-white text-gray-800 bg-[url('/assets/background.png')] bg-cover bg-center bg-no-repeat bg-fixed min-h-screen">
+      <body className="font-sans bg-[#111] text-gray-800  bg-center bg-no-repeat bg-fixed min-h-screen">
+        {/*bg-[url('/assets/background.png')] bg-cover*/}
         <Navbar />
         <main className="pt-1">{children}</main>
         {/* bg-[rgb(223,222,222)]*/}
