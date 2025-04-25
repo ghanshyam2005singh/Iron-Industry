4d0bb979e8cbccd21349803151db457d5cce6ea4
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-04-25 15:26:18 +0530

    theme change button added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
-import { Navbar } from '../components/Navbar';
-import { Footer } from '../components/Footer';
-import "./globals.css";
-
+import './globals.css'
+import { Navbar } from '../components/Navbar'
+import { Footer } from '../components/Footer'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className="font-sans bg-[#111] text-gray-800  bg-center bg-no-repeat bg-fixed min-h-screen">
-        {/*bg-[url('/assets/background.png')] bg-cover*/}
+    <html lang="en" suppressHydrationWarning>
+      <body className="bg-[rgb(223,222,222)] bg-[url('/assets/background.png')] bg-cover dark:bg-black dark:bg-none text-black dark:text-white transition-colors duration-300">
         <Navbar />
-        <main className="pt-1">{children}</main>
-        {/* bg-[rgb(223,222,222)]*/}
+        <main className="pt-20">{children}</main>
         <Footer />
       </body>
     </html>
