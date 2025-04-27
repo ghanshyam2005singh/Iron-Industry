f690d01507afd089db8626af6b13527f897a1f42
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-04-27 18:07:59 +0530

    page update

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,25 @@ import Products from './products/page';
 import Contact from './contact/page';
 import "./globals.css";
 import About from './about/page';
-import Team from "./team/page";
+import TeamSection from "./team/page";
+import TechStackSlider from './tech-stack/page';
 
 export default function HomePage() {
   return (
-    <section className="bg-black text-white px-4 space-y-28">
-      <section className="min-h-screen flex items-center justify-center">
+    <section className="px-4 space-y-28 bg-[rgb(223,222,222)] bg-[url('/assets/background.png')] bg-cover dark:bg-black dark:bg-none text-black dark:text-white transition-all duration-300">
+      <section className="flex items-center justify-center">
         <About />
       </section>
-      <section className="min-h-screen flex items-center justify-center">
+      <section className="flex items-center justify-center">
+        <TechStackSlider />
+      </section>
+      <section className="flex items-center justify-center">
         <Products />
       </section>
-      <section className="min-h-screen flex items-center justify-center">
-        <Team />
+      <section className="flex items-center justify-center">
+        <TeamSection />
       </section>
-      <section className="min-h-screen flex items-center justify-center">
+      <section className="flex items-center justify-center">
         <Contact />
       </section>
     </section>
