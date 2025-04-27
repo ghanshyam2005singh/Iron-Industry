e1c0a40cf0096f1127495af86af979414ff183cf
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-04-27 18:08:26 +0530

    page update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-      <footer className="py-6 mt-10 border-t text-center text-sm bg-[#0A0F1C] text-gray-500">
+      <footer className="py-6 mt-10 border-t text-center text-sm  text-gray-500">
         © {new Date().getFullYear()} Iron Industry. All rights reserved.
       </footer>
     );
