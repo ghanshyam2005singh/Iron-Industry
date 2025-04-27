bd3826c21f51359787ae6c1e22ec594d26ab722e
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-04-27 18:07:48 +0530

    page update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
-import './globals.css'
-import { Navbar } from '../components/Navbar'
-import { Footer } from '../components/Footer'
+import './globals.css';
+import { Navbar } from '../components/Navbar';
+import { Footer } from '../components/Footer';
+import { ThemeProvider } from '../components/ThemeProvider'; // We'll create it too
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="bg-[rgb(223,222,222)] bg-[url('/assets/background.png')] bg-cover dark:bg-black dark:bg-none text-black dark:text-white transition-colors duration-300">
-        <Navbar />
-        <main className="pt-20">{children}</main>
-        <Footer />
+      <body className="transition-colors duration-300">
+        <ThemeProvider>
+          <Navbar />
+          <main className="pt-20 bg-[rgb(223,222,222)] bg-[url('/assets/background.png')] bg-cover dark:bg-black dark:bg-none text-black dark:text-white min-h-screen">
+            {children}
+          </main>
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
