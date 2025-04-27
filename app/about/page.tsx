4beecd8d7a97b9e2eb2748e5c414b7960caac492
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-04-27 18:06:11 +0530

    page update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 export default function AboutPage() {
   return (
-    <section id='about' className="min-h-screen bg-black flex flex-col items-center justify-center py-10 px-4 text-white">
-      <div className="flex flex-col items-center justify-center bg-[#111] rounded-2xl border border-zinc-800 shadow-[0_0_40px_-10px_#00f0ff] p-8 max-w-4xl w-full">
+    <section id='about' className="bg-[rgb(223,222,222)] bg-[url('/assets/background.png')] bg-cover dark:bg-black dark:bg-none text-black dark:text-white transition-all duration-300 flex flex-col items-center justify-center py-10 px-4">
+      <div className="flex flex-col items-center justify-center rounded-2xl border dark:border-zinc-800 shadow-[0_0_40px_-10px_#00f0ff] p-8 max-w-4xl w-full">
         {/* Header */}
         <h1 className="text-4xl md:text-6xl font-extrabold font-mono bg-gradient-to-r from-amber-400 via-pink-500 to-violet-600 bg-clip-text text-transparent text-center tracking-tight">
           Welcome to Iron Industry
@@ -24,7 +24,7 @@ export default function AboutPage() {
 
       {/* Cool Quote */}
       <div className="text-center mt-12 max-w-2xl">
-        <p className="text-lg md:text-xl text-zinc-400 italic font-light">
+        <p className="text-lg md:text-xl text-black dark:text-white italic font-light">
           We don’t just build products — we build legends, write code like wizards, and ship like pirates. 🏴‍☠️
         </p>
 
