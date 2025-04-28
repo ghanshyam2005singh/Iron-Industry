012282e65fa5c5326fb5dd43de6ab162d259e34c
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-04-28 14:38:55 +0530

    extra space problem solved

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-      <footer className="py-6 mt-10 border-t text-center text-sm  text-gray-500">
+      <footer className="py-10 bg-[rgb(223,222,222)] bg-[url('/assets/background.png')] bg-cover dark:bg-black dark:bg-none dark:text-white transition-all duration-300  border-t text-center text-sm  text-gray-500">
         © {new Date().getFullYear()} Iron Industry. All rights reserved.
       </footer>
     );
