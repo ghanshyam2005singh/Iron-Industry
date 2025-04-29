8b87c07a010aa8da22abaa7b19272a5eb74fba06
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-04-29 11:48:12 +0530

    url removed

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,8 +6,8 @@ export default function Products() {
     {
       name: "AlumConnect",
       description: "The networking revolution for students & alumni.",
-      link: "https://alum-connect-xi.vercel.app/",
-      status: "Live 🚀",
+      link: "https://",
+      status: "Live Soon🚀",
     },
     {
       name: "Homie-Finder",
