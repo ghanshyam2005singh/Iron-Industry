0e21ce54f7788428f17bde62cd94ee59f5224bea
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-04-30 21:38:50 +0530

    defaullt dark implement

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from '../components/ThemeProvider'; // We'll create it 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="dark">
       <body className="transition-colors duration-300">
         <ThemeProvider>
           <Navbar />
